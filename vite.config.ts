77fe7408e370c5e15651868820d2cb79e83a4b78
matteofinco <matteofinco05@gmail.com>
2026-06-05 02:07:21 +0200

    Update vite.config.tsh

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { createServer } from "./server";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/builderio/", // <-- 1. CORREZIONE: Imposta il percorso corretto per GitHub Pages
+  base: "/", // <-- 1. CORREZIONE: Imposta il percorso corretto per GitHub Pages
   server: {
     host: "::",
     port: 8080,
